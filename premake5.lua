e34a3278fa0e0ddb1d7976e86dc3d23843606929
Filip Vondrášek <filip@vondrasek.net>
2025-01-03 16:11:32 +0100

    premake now makes sure the DLLs the project is dependent on get copied to the bin directory after build.

--- premake5.lua
+++ premake5.lua
@@ -11,6 +11,15 @@ project "Sorter"
 	libdirs { "%{prj.name}/dependencies/SFML-2.6.2/lib", "%{prj.name}/dependencies/SDL-2.30.10/lib/x64" }
 	links { "sfml-graphics.lib", "sfml-window.lib", "sfml-system.lib", "kernel32.lib", "user32.lib", "gdi32.lib", "winspool.lib", "comdlg32.lib", "advapi32.lib", "shell32.lib", "ole32.lib", "oleaut32.lib", "uuid.lib", "odbc32.lib", "odbccp32.lib", "SDL2.lib", "SDL2main.lib", "SDL2test.lib" }
 
+	filter "system:windows"
+		postbuildcommands
+        	{
+            		{ "{COPY} %{prj.name}/dependencies/SDL-2.30.10/lib/x64/SDL2.dll bin/%{cfg.buildcfg}" },
+			{ "{COPY} %{prj.name}/dependencies/SFML-2.6.2/bin/sfml-graphics-2.dll bin/%{cfg.buildcfg}" },
+			{ "{COPY} %{prj.name}/dependencies/SFML-2.6.2/bin/sfml-system-2.dll bin/%{cfg.buildcfg}" },			
+			{ "{COPY} %{prj.name}/dependencies/SFML-2.6.2/bin/sfml-window-2.dll bin/%{cfg.buildcfg}" },
+     		}
+
 	filter "configurations:Debug"
 		defines { "DEBUG" }
 		architecture "x64"
