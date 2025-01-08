5e69e646fa338b1141e424ad1602ffe5c19b566b
Filip Vondrasek <filip@vondrasek.net>
2025-01-08 12:49:06 +0100

    Fixed SFML release library being linked against the debug builds.

--- premake5.lua
+++ premake5.lua
@@ -9,17 +9,13 @@ project "Sorter"
 	files { "src/**.cpp", "src/**.h" }
 	includedirs { "dependencies/SFML-2.6.2/include", "dependencies/SDL-2.30.10/include" }
 	libdirs { "dependencies/SFML-2.6.2/lib", "dependencies/SDL-2.30.10/lib/x64" }
-	links { "sfml-graphics.lib", "sfml-window.lib", "sfml-system.lib", "sfml-audio.lib", "SDL2.lib", "SDL2_ttf.lib", "SDL2main.lib", "SDL2test.lib" }
+	links { "SDL2.lib", "SDL2_ttf.lib", "SDL2main.lib", "SDL2test.lib" }
 
 	filter "system:windows"
 		postbuildcommands
 		{
 			{ "{COPY} dependencies/SDL-2.30.10/lib/x64/SDL2.dll bin/%{cfg.buildcfg}" },
 			{ "{COPY} dependencies/SDL-2.30.10/lib/x64/SDL2_ttf.dll bin/%{cfg.buildcfg}" },
-			{ "{COPY} dependencies/SFML-2.6.2/bin/sfml-graphics-2.dll bin/%{cfg.buildcfg}" },
-			{ "{COPY} dependencies/SFML-2.6.2/bin/sfml-system-2.dll bin/%{cfg.buildcfg}" },			
-			{ "{COPY} dependencies/SFML-2.6.2/bin/sfml-window-2.dll bin/%{cfg.buildcfg}" },
-			{ "{COPY} dependencies/SFML-2.6.2/bin/sfml-audio-2.dll bin/%{cfg.buildcfg}" }
 		}
 
 	filter "configurations:Debug"
@@ -27,9 +23,25 @@ project "Sorter"
 		architecture "x64"
 		symbols "On"
 		cppdialect "C++latest"
+		links { "sfml-graphics-d.lib", "sfml-window-d.lib", "sfml-system-d.lib", "sfml-audio-d.lib" }
+		postbuildcommands
+		{	
+			{ "{COPY} dependencies/SFML-2.6.2/bin/sfml-graphics-d-2.dll bin/%{cfg.buildcfg}" },
+			{ "{COPY} dependencies/SFML-2.6.2/bin/sfml-system-d-2.dll bin/%{cfg.buildcfg}" },			
+			{ "{COPY} dependencies/SFML-2.6.2/bin/sfml-window-d-2.dll bin/%{cfg.buildcfg}" },
+			{ "{COPY} dependencies/SFML-2.6.2/bin/sfml-audio-d-2.dll bin/%{cfg.buildcfg}" }
+		}
 
 	filter "configurations:Release"
 		defines { "NDEBUG" }
 		architecture "x64"
 		optimize "On"
 		cppdialect "C++latest"
+		links { "sfml-graphics.lib", "sfml-window.lib", "sfml-system.lib", "sfml-audio.lib" }
+		postbuildcommands
+		{	
+			{ "{COPY} dependencies/SFML-2.6.2/bin/sfml-graphics-2.dll bin/%{cfg.buildcfg}" },
+			{ "{COPY} dependencies/SFML-2.6.2/bin/sfml-system-2.dll bin/%{cfg.buildcfg}" },			
+			{ "{COPY} dependencies/SFML-2.6.2/bin/sfml-window-2.dll bin/%{cfg.buildcfg}" },
+			{ "{COPY} dependencies/SFML-2.6.2/bin/sfml-audio-2.dll bin/%{cfg.buildcfg}" }
+		}
